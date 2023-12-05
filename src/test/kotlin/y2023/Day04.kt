41eb2b1acd0945de08e31ab1d62eedf5561413b3
Mike Lamsma <mikelamsma@gmail.com>
2023-12-05 20:54:38 +0100

    Day 04 ⭐

---
+++ src/test/kotlin/y2023/Day04.kt
@@ -0,0 +1,73 @@
+package y2023
+
+import util.getInputAsLines
+import util.parseExampleInput
+import java.util.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+private fun task1(inputLines: List<String>) =
+    inputLines
+        .sumOf { card ->
+            val numbers = card.substringAfter(": ").trim().replace("\\s+".toRegex(), " ").split(" | ")
+            val (winningNumbers, scratchedNumbers) = pairOfNumbers(numbers)
+
+            var points = 0
+            winningNumbers.forEach { winningNumber ->
+                if (scratchedNumbers.contains(winningNumber)) {
+                    if (points == 0) {
+                        points++
+                    } else {
+                        points *= 2
+                    }
+                }
+            }
+            points
+        }
+
+private fun task2(inputLines: List<String>) = 1
+
+private fun pairOfNumbers(numbers: List<String>) =
+    parseNumbers(numbers.first()) to parseNumbers(numbers.last())
+
+private fun parseNumbers(numbersString: String) = numbersString.split(" ").map { it.toInt() }
+
+class Day04 {
+    private val input = getInputAsLines("2023", this::class.simpleName!!.lowercase(Locale.getDefault()))
+
+    @Test
+    fun testTask1ExampleInput() {
+        val expected = 13
+        val input = parseExampleInput(exampleInput)
+        assertEquals(expected, task1(input))
+    }
+
+    @Test
+    fun testTask1ActualInput() {
+        val expected = 27059
+        assertEquals(expected, task1(input))
+    }
+
+    @Test
+    fun testTask2ExampleInput() {
+        val expected = 30
+        val input = parseExampleInput(exampleInput)
+        assertEquals(expected, task2(input))
+    }
+
+    @Test
+    fun testTask2ActualInput() {
+        val expected = 0
+        assertEquals(expected, task2(input))
+    }
+}
+
+private val exampleInput =
+    """
+        Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+        Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+        Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+        Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+        Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+        Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11
+    """.trimIndent()
